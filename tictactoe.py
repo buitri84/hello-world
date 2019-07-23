3c64f9aaca707a8051306c7cee4c06baf39ab0fc
buitri84 <buitri84@gmail.com>
2019-07-23 10:09:54 +0000

    Board data structure streamlined. Also, board data structure in main, not global anymore

--- tictactoe.py
+++ tictactoe.py
@@ -1,26 +1,29 @@
-# Declare board, set of winning states, and initialize state vector for each player
-board = []
-for i in range(9):
-    board.append({"occupied": False, "player": 0})
-winning = [{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}]
-player_x = set()
-player_o = set()
 
-# This function checks if a move is legal and updates the board, also updates the state vector of each player, then returns true or false
-def move(pos, player):
-    if board[pos]["occupied"] == True:
+# Declare set of winning states
+winning = [{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}]
+
+# This function checks if a move is legal and updates the board
+def move(board, pos, player):
+    if board[pos] != 0:
         return False
     else:
-        board[pos]["occupied"] = True
-        board[pos]["player"] = player
         if player == 1:
-            player_x.add(pos)
+            board[pos] = 1
         elif player == -1:
-            player_o.add(pos)
+            board[pos] = -1
         return True
 
-# This function checks the state of the game and return the score (1 if player 1 wins, -1 if player -1 wins, and 0 if it is a draw or incomplete)
-def gameover():
+# This function checks the state of the game and return the score (1 if player 1 wins, -1 if player -1 wins, and 0 if it is a draw)
+# an incomplete game returns False, while completed game returns True
+def gameover(board):
+    # Create 2 vectors for state of players and populate them
+    player_x = set()
+    player_o = set()
+    for i in range(9):
+        if board[i] == 1:
+            player_x.add(i)
+        elif board[i] == -1:
+            player_o.add(i)
     print(player_x)
     print(player_o)
     # Check if any player has winning combination
@@ -31,17 +34,21 @@ def gameover():
             return True, -1
     # If not, then it's either just an incomplete game or a draw
     for i in board:
-        if i["occupied"] == False:
+        if i == 0:
             return False, 0
     return True, 0
 
-
 def main():
+    # Declare board. Note: Actually I just need an array, no need for complicated structure
+    board = []
+    for i in range(9):
+        board.append(0)
+
     # Set flag for starting player
     flag = 1
     # Loop to keep getting input where to play
     while True:
-        state, score = gameover()
+        state, score = gameover(board)
         print(state, score)
         if state == True:
             if score == 1:
@@ -57,7 +64,7 @@ def main():
         if pos == -1:
             break
         else:
-            mov = move(pos, flag)
+            mov = move(board, pos, flag)
             # if the move has succeded, switch player (flag), otherwise do nothing ie. current player moves again
             if mov == True:
                 flag = -1* flag
